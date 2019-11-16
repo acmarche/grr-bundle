a0d82a9c418b286380483b63b83a1243e4687fbd
jfsenechal <jfsenechal@gmail.com>
2019-11-16 20:48:50 +0100

    init

---
+++ GrrBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of sf5 application
+ * @author jfsenechal <jfsenechal@gmail.com>
+ * @date 16/11/19
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace Grr\GrrBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GrrBundle extends Bundle
+{
+
+}
