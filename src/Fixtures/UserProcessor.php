03bd03de6c7b7212b8ac4911d0bb77639d187b1f
jfsenechal <jfsenechal@gmail.com>
2019-12-05 20:37:54 +0100

    tests

---
+++ src/Fixtures/UserProcessor.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of GrrSf application.
+ *
+ * @author jfsenechal <jfsenechal@gmail.com>
+ * @date 21/08/19
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Grr\GrrBundle\Fixtures;
+
+use Fidry\AliceDataFixtures\ProcessorInterface;
+use Grr\GrrBundle\Entity\Security\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserProcessor implements ProcessorInterface
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function preProcess(string $fixtureId, $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setPassword($this->userPasswordEncoder->encodePassword($user, $user->getPassword()));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postProcess(string $fixtureId, $user): void
+    {
+        // do nothing
+    }
+}
