ea7a9c19efe4034da97bd3bd2dd99ea365c1661b
jfsenechal <jfsenechal@gmail.com>
2020-10-05 12:05:01 +0200

    vue select room and display entries

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -2,18 +2,35 @@
 
 namespace Grr\GrrBundle\Controller;
 
+use Grr\Core\Contrat\Repository\AreaRepositoryInterface;
+use Grr\Core\Contrat\Repository\EntryRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VueController extends AbstractController
 {
     /**
+     * @var EntryRepositoryInterface
+     */
+    private $entryRepository;
+    /**
+     * @var AreaRepositoryInterface
+     */
+    private $areaRepository;
+
+    public function __construct(EntryRepositoryInterface $entryRepository, AreaRepositoryInterface $areaRepository)
+    {
+        $this->entryRepository = $entryRepository;
+        $this->areaRepository = $areaRepository;
+    }
+
+    /**
      * @Route("/vue", name="vue")
      */
     public function index()
     {
         return $this->render('vue/index.html.twig', [
-            'controller_name' => 'VueController',
+            'areas' => $this->areaRepository->findAll(),
         ]);
     }
 }
--- src/Entity/Entry.php
+++ src/Entity/Entry.php
@@ -4,18 +4,56 @@ namespace Grr\GrrBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Grr\Core\Contrat\Entity\EntryInterface;
+use Grr\Core\Contrat\Entity\RoomInterface;
 use Grr\Core\Entry\Entity\EntryTrait;
 use Grr\GrrBundle\Entry\Validator as GrrEntryAssert;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Table(name="entry")
  * @ORM\Entity(repositoryClass="Grr\GrrBundle\Entry\Repository\EntryRepository")
  * @GrrEntryAssert\BusyRoom
  * @GrrEntryAssert\AreaTimeSlot
+ *
+ * @ApiResource(
+ *     normalizationContext={"groups"={"entry:read"}},
+ *     denormalizationContext={"groups"={"entry:write"}},
+ *     itemOperations={"get"}
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"nom": "partial", "id": "exact", "room": "exact"})
  */
 class Entry implements EntryInterface, TimestampableInterface
 {
     use EntryTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @Groups("entry:read")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=80, nullable=false)
+     * @Groups("entry:read")
+     */
+    private $name;
+
+    /**
+     * @var RoomInterface
+     * @ORM\ManyToOne(targetEntity="Grr\Core\Contrat\Entity\RoomInterface", inversedBy="entries")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups("entry:read")
+     */
+    private $room;
 }
