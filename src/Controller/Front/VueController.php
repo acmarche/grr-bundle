c98f4cf4e72f42ef05fde6fa1503b288daec7fc7
jfsenechal <jfsenechal@gmail.com>
2021-09-14 19:56:36 +0200

    fix paramconverter

--- src/Controller/Front/VueController.php
+++ src/Controller/Front/VueController.php
@@ -8,6 +8,7 @@ use Grr\Core\View\ViewLocator;
 use Grr\GrrBundle\Entity\Area;
 use Grr\GrrBundle\Entity\Room;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,10 +29,8 @@ class VueController extends AbstractController implements FrontControllerInterfa
 
     /**
      * @Route("/area/{area}/date/{date}/view/{view}/room/{room}", name="grr_front_view", methods={"GET"})
-     * Route("/area/{area}/date/{date}/view/{view}/room/", name="grr_front_view", methods={"GET"})
-     *
      * @Entity("area", expr="repository.find(area)")
-     * @Entity("room", expr="repository.find(room)", isOptional=true)
+     * @ParamConverter("room", class="Grr\GrrBundle\Entity\Room", isOptional=true, options={"id"="room"})
      * @throws Exception
      */
     public function view(Area $area, DateTime $date, string $view, ?Room $room = null): Response
