da8f2e0194067d2ed31a2bda9c7dd84a4282c934
jfsenechal <jfsenechal@gmail.com>
2021-12-06 16:54:34 +0100

    add guest view

---
+++ src/Controller/Front/GuestController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Grr\GrrBundle\Controller\Front;
+
+use DateTime;
+use Grr\Core\Contrat\Repository\EntryRepositoryInterface;
+use Grr\Core\Entry\Message\EntryCreated;
+use Grr\Core\Router\FrontRouterHelper;
+use Grr\GrrBundle\Entity\Entry;
+use Grr\GrrBundle\Entity\Room;
+use Grr\GrrBundle\Entry\Factory\EntryFactory;
+use Grr\GrrBundle\Entry\Form\EntryGuestWithPeriodicityType;
+use Grr\GrrBundle\Entry\HandlerEntry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @Route("/front/guest")
+ */
+class GuestController extends AbstractController
+{
+    private EntryRepositoryInterface $entryRepository;
+    private EntryFactory $entryFactory;
+    private HandlerEntry $handlerEntry;
+    private EventDispatcherInterface $eventDispatcher;
+    private FrontRouterHelper $frontRouterHelper;
+
+    public function __construct(
+        EntryFactory $entryFactory,
+        EntryRepositoryInterface $entryRepository,
+        HandlerEntry $handlerEntry,
+        EventDispatcherInterface $eventDispatcher,
+        FrontRouterHelper $frontRouterHelper
+    ) {
+        $this->entryRepository = $entryRepository;
+        $this->entryFactory = $entryFactory;
+        $this->handlerEntry = $handlerEntry;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->frontRouterHelper = $frontRouterHelper;
+    }
+
+    /**
+     * @Route("/new/room/{id}", name="grr_front_guest_new", methods={"GET", "POST"})
+     */
+    public function new(
+        Request $request,
+        Room $room
+    ): Response {
+        $area = $room->getArea();
+        $date = new DateTime();
+        $entry = $this->entryFactory->initEntryForNew($area, $room, $date, 8, 0);
+        $form = $this->createForm(EntryGuestWithPeriodicityType::class, $entry);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->handlerEntry->handleNewEntry($entry);
+
+            $this->dispatchMessage(new EntryCreated($entry->getId()));
+
+            return $this->redirectToRoute('grr_front_entry_show', ['id' => $entry->getId()]);
+        }
+
+        return $this->render(
+            '@grr_front/guest/new.html.twig',
+            [
+                'entry' => $entry,
+                'room' => $room,
+                'periodicity' => null,
+                'displayOptionsWeek' => false,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="grr_front_guest_show", methods={"GET"})
+     * @IsGranted("grr.entry.show", subject="entry")
+     */
+    public function show(Entry $entry): Response
+    {
+        $urlList = $this->frontRouterHelper->generateMonthView($entry);
+        $repeats = [];
+        if (null !== ($periodicity = $entry->getPeriodicity())) {
+            $repeats = $this->entryRepository->findByPeriodicity($periodicity);
+        }
+
+        return $this->render(
+            '@grr_front/guest/show.html.twig',
+            [
+                'entry' => $entry,
+                'repeats' => $repeats,
+                'url_back' => $urlList,
+            ]
+        );
+    }
+
+}
---
+++ src/Entry/Form/EntryGuestWithPeriodicityType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Grr\GrrBundle\Entry\Form;
+
+use Grr\Core\Factory\DurationFactory;
+use Grr\GrrBundle\Entity\Entry;
+use Grr\GrrBundle\EventSubscriber\Form\AddDurationFieldSubscriber;
+use Grr\GrrBundle\Periodicity\Form\PeriodicityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntryGuestWithPeriodicityType extends AbstractType
+{
+    private DurationFactory $durationFactory;
+
+    public function __construct(DurationFactory $durationFactory)
+    {
+        $this->durationFactory = $durationFactory;
+    }
+
+    public function buildForm(FormBuilderInterface $formBuilder, array $options): void
+    {
+        $formBuilder
+            ->add(
+                'startTime',
+                DateTimeType::class,
+                [
+                    'label' => 'label.entry.startTime',
+                    'help' => 'help.entry.startTime',
+                ]
+            )
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'label.entry.name',
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'label' => 'label.entry.description',
+                    'required' => false,
+                ]
+            )
+            ->addEventSubscriber(new AddDurationFieldSubscriber($this->durationFactory))
+            ->add('periodicity', PeriodicityType::class);
+    }
+
+    public function configureOptions(OptionsResolver $optionsResolver): void
+    {
+        $optionsResolver->setDefaults(
+            [
+                'data_class' => Entry::class,
+            ]
+        );
+    }
+
+
+}
