a3a0647e333b22983315e03c79ecc0b125ed36cc
jfsenechal <jfsenechal@gmail.com>
2022-06-28 16:31:30 +0200

    test component

---
+++ src/Twig/Component/AlertComponent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Grr\GrrBundle\Twig\Component;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('alert', template: '@grr_front/components/alert.html.twig')]
+class AlertComponent
+{
+    public string $type = 'success';
+    public string $message;
+
+    public function __construct()
+    {
+    }
+
+    public function getIconClass(): string
+    {
+        return match ($this->type) {
+            'success' => 'fa fa-circle-check',
+            'danger' => 'fa fa-circle-exclamation',
+        };
+    }
+
+    public function getPackageCount(): int
+    {
+        return 5;
+    }
+}
