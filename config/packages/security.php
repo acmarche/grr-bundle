57d3d226dc0d54e2080357a38821aa8cce4f8421
jfsenechal <jfsenechal@gmail.com>
2024-04-03 17:33:17 +0200

    up

--- config/packages/security.php
+++ config/packages/security.php
@@ -4,35 +4,19 @@ use Grr\Core\Security\SecurityRole;
 use Grr\GrrBundle\Entity\Security\User;
 use Grr\GrrBundle\Security\Authenticator\GrrAuthenticator;
 use Grr\GrrBundle\Security\Authenticator\GrrLdapAuthenticator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Ldap\Ldap;
 use Symfony\Component\Ldap\LdapInterface;
+use Symfony\Config\SecurityConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('security', [
-        'password_hashers' => [
-            User::class => [
-                'algorithm' => 'auto',
-            ],
-        ],
-    ]);
+return static function (SecurityConfig $security) {
 
-    $containerConfigurator->extension(
-        'security',
-        [
-            'providers' => [
-                'grr_user_provider' => [
-                    'entity' => [
-                        'class' => User::class,
-                        'property' => 'username',
-                    ],
-                ],
-            ],
-        ]
-    );
+    $security
+        ->provider('grr_user_provider')
+        ->entity()
+        ->class(User::class)
+        ->property('username');
 
-    $authenticators = [GrrAuthenticator::class];
-
+    // @see Symfony\Config\Security\FirewallConfig
     $main = [
         'provider' => 'grr_user_provider',
         'logout' => [
@@ -40,7 +24,30 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
         'form_login' => [],
         'entry_point' => GrrAuthenticator::class,
+        'login_throttling' => [
+            'max_attempts' => 6, // per minute...
+        ],
+        'remember_me' => [
+            'secret' => '%kernel.secret%',
+            'lifetime' => 604800,
+            'path' => '/',
+            'always_remember_me' => true,
+        ],
     ];
+
+    $t =
+        [
+            SecurityRole::ROLE_GRR_ADMINISTRATOR => [
+                SecurityRole::ROLE_GRR,
+                SecurityRole::ROLE_GRR_MANAGER_USER,
+                SecurityRole::ROLE_GRR_BOOKING,
+            ],
+            SecurityRole::ROLE_GRR_ACTIVE_USER => [SecurityRole::ROLE_GRR],
+            SecurityRole::ROLE_GRR_MANAGER_USER => [SecurityRole::ROLE_GRR],
+            SecurityRole::ROLE_GRR_BOOKING => [SecurityRole::ROLE_GRR],
+        ];
+
+    $authenticators = [GrrAuthenticator::class];
 
     if (interface_exists(LdapInterface::class)) {
         $authenticators[] = GrrLdapAuthenticator::class;
@@ -50,30 +57,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ];
     }
 
-    $main['custom_authenticator'] = $authenticators;
-
-    $containerConfigurator->extension(
-        'security',
-        [
-            'firewalls' => [
-                'main' => $main,
-            ],
-        ]
-    );
-
-    $containerConfigurator->extension(
-        'security',
-        [
-            'role_hierarchy' => [
-                SecurityRole::ROLE_GRR_ADMINISTRATOR => [
-                    SecurityRole::ROLE_GRR,
-                    SecurityRole::ROLE_GRR_MANAGER_USER,
-                    SecurityRole::ROLE_GRR_BOOKING,
-                ],
-                SecurityRole::ROLE_GRR_ACTIVE_USER => [SecurityRole::ROLE_GRR],
-                SecurityRole::ROLE_GRR_MANAGER_USER => [SecurityRole::ROLE_GRR],
-                SecurityRole::ROLE_GRR_BOOKING => [SecurityRole::ROLE_GRR],
-            ],
-        ]
-    );
+    $main['custom_authenticators'] = $authenticators;
+    $security
+        ->roleHierarchy(SecurityRole::ROLE_GRR_ADMINISTRATOR, [
+            SecurityRole::ROLE_GRR,
+            SecurityRole::ROLE_GRR_MANAGER_USER,
+            SecurityRole::ROLE_GRR_BOOKING,
+        ])
+        ->roleHierarchy(SecurityRole::ROLE_GRR_ACTIVE_USER, [SecurityRole::ROLE_GRR])
+        ->roleHierarchy(SecurityRole::ROLE_GRR_MANAGER_USER, [SecurityRole::ROLE_GRR])
+        ->roleHierarchy(SecurityRole::ROLE_GRR_BOOKING, [SecurityRole::ROLE_GRR])
+        ->firewall('main', $main);
 };
+
